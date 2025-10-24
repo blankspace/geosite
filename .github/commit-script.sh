f60ac598c988ce93cbb990326d1e9780e00b4d8c
blankspace <jessechan118@gmail.com>
2025-10-24 23:54:46 +0800

    Change remote repository URL in commit script

--- .github/commit-script.sh
+++ .github/commit-script.sh
@@ -6,7 +6,7 @@ cd output
 git init
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lhear/geosite.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/blankspace/geosite.git
 git branch -M release
 git add .
 git commit -m "Update release"
